51aed920a1c4c8752a4690db76eb0f1c2ae64973
micufx <micualtamirano@hotmail.com>
2025-01-14 15:41:01 +0100

    Update BIDS_converter_eeg_motion.m

--- scripts/BIDS_converter_eeg_motion.m
+++ scripts/BIDS_converter_eeg_motion.m
@@ -124,7 +124,7 @@ for sub = 1: length(files)
     
 
     % Generate README file
-    README = sprintf('The experiment included 27 participants. \n- Miguel Contreras-Altamirano (December, 2024)');
+    README = sprintf('The experiment included 27 participants. \n- Miguel Contreras-Altamirano (December, 2025)');
 
     % Call data2bids for EEG
     data2bids(cfg, data_eeg);
